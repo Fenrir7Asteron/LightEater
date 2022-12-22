c3a8e3b15333b11e919eea8438521f5931b16111
Fenrir7Asteron <b.fedotov@innopolis.ru>
2022-12-22 23:25:42 +0300

    Add enemy and destructible objects hit reaction. Add stun to enemies. Fix player attack montage and add hit connection pause.

---
+++ Plugins/FCTween/Intermediate/Build/Win64/UnrealEditor/Inc/FCTween/FCTweenSubsystem.generated.h
@@ -0,0 +1,92 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef FCTWEEN_FCTweenSubsystem_generated_h
+#error "FCTweenSubsystem.generated.h already included, missing '#pragma once' in FCTweenSubsystem.h"
+#endif
+#define FCTWEEN_FCTweenSubsystem_generated_h
+
+#define FID_LightEater_Plugins_FCTween_Source_FCTween_Public_FCTweenSubsystem_h_8_SPARSE_DATA
+#define FID_LightEater_Plugins_FCTween_Source_FCTween_Public_FCTweenSubsystem_h_8_RPC_WRAPPERS
+#define FID_LightEater_Plugins_FCTween_Source_FCTween_Public_FCTweenSubsystem_h_8_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_LightEater_Plugins_FCTween_Source_FCTween_Public_FCTweenSubsystem_h_8_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUFCTweenSubsystem(); \
+	friend struct Z_Construct_UClass_UFCTweenSubsystem_Statics; \
+public: \
+	DECLARE_CLASS(UFCTweenSubsystem, UGameInstanceSubsystem, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/FCTween"), NO_API) \
+	DECLARE_SERIALIZER(UFCTweenSubsystem)
+
+
+#define FID_LightEater_Plugins_FCTween_Source_FCTween_Public_FCTweenSubsystem_h_8_INCLASS \
+private: \
+	static void StaticRegisterNativesUFCTweenSubsystem(); \
+	friend struct Z_Construct_UClass_UFCTweenSubsystem_Statics; \
+public: \
+	DECLARE_CLASS(UFCTweenSubsystem, UGameInstanceSubsystem, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/FCTween"), NO_API) \
+	DECLARE_SERIALIZER(UFCTweenSubsystem)
+
+
+#define FID_LightEater_Plugins_FCTween_Source_FCTween_Public_FCTweenSubsystem_h_8_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UFCTweenSubsystem(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UFCTweenSubsystem) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UFCTweenSubsystem); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UFCTweenSubsystem); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UFCTweenSubsystem(UFCTweenSubsystem&&); \
+	NO_API UFCTweenSubsystem(const UFCTweenSubsystem&); \
+public:
+
+
+#define FID_LightEater_Plugins_FCTween_Source_FCTween_Public_FCTweenSubsystem_h_8_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UFCTweenSubsystem() { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UFCTweenSubsystem(UFCTweenSubsystem&&); \
+	NO_API UFCTweenSubsystem(const UFCTweenSubsystem&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UFCTweenSubsystem); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UFCTweenSubsystem); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UFCTweenSubsystem)
+
+
+#define FID_LightEater_Plugins_FCTween_Source_FCTween_Public_FCTweenSubsystem_h_5_PROLOG
+#define FID_LightEater_Plugins_FCTween_Source_FCTween_Public_FCTweenSubsystem_h_8_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_LightEater_Plugins_FCTween_Source_FCTween_Public_FCTweenSubsystem_h_8_SPARSE_DATA \
+	FID_LightEater_Plugins_FCTween_Source_FCTween_Public_FCTweenSubsystem_h_8_RPC_WRAPPERS \
+	FID_LightEater_Plugins_FCTween_Source_FCTween_Public_FCTweenSubsystem_h_8_INCLASS \
+	FID_LightEater_Plugins_FCTween_Source_FCTween_Public_FCTweenSubsystem_h_8_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_LightEater_Plugins_FCTween_Source_FCTween_Public_FCTweenSubsystem_h_8_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_LightEater_Plugins_FCTween_Source_FCTween_Public_FCTweenSubsystem_h_8_SPARSE_DATA \
+	FID_LightEater_Plugins_FCTween_Source_FCTween_Public_FCTweenSubsystem_h_8_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_LightEater_Plugins_FCTween_Source_FCTween_Public_FCTweenSubsystem_h_8_INCLASS_NO_PURE_DECLS \
+	FID_LightEater_Plugins_FCTween_Source_FCTween_Public_FCTweenSubsystem_h_8_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> FCTWEEN_API UClass* StaticClass<class UFCTweenSubsystem>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_LightEater_Plugins_FCTween_Source_FCTween_Public_FCTweenSubsystem_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
